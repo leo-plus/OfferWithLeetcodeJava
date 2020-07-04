45911fffbfe977d902c917ebb2e76d0151e4b4ac
leo_plus <lizhaojunleo@gmail.com>
2020-07-04 10:44:56 +0800

    1

---
+++ Solution43.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countDigitOne(int n) {
+        return f(n);
+    }
+
+    private int f(int n) {
+        if (n <= 0)
+            return 0;
+        String s = String.valueOf(n);
+        int high = s.charAt(0) - '0';
+        int pow = (int) Math.pow(10, s.length() - 1);
+        int last = n - high * pow;
+        if (high == 1) {
+            return f(pow - 1) + last + 1 + f(last);
+        } else {
+            return pow + high * f(pow - 1) + f(last);
+        }
+    }
+}
+
+// 链接：https://leetcode-cn.com/problems/1nzheng-shu-zhong-1chu-xian-de-ci-shu-lcof/solution/javadi-gui-by-xujunyi/
