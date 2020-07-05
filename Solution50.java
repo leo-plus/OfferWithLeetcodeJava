0d9c4db50f04e6da0c392150f27c61f49f94c308
leo_plus <lizhaojunleo@gmail.com>
2020-07-05 22:44:51 +0800

    1

---
+++ Solution50.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+class Solution {
+    public char firstUniqChar(String s) {
+        HashMap<Character, Boolean> dic = new HashMap<>();
+        char[] sc = s.toCharArray();
+        for (char c : sc) {
+            dic.put(c, !dic.containsKey(c));
+        }
+
+        for (char c : sc) {
+            if (dic.get(c)) {
+                return c;
+            }
+        }
+
+        return ' ';
+    }
+}
+
+// https://leetcode-cn.com/problems/di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof/solution/mian-shi-ti-50-di-yi-ge-zhi-chu-xian-yi-ci-de-zi-3/
