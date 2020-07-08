d5d1fbf6d247a4b9c9d7d18c078c44dc5ac9f6c7
leo_plus <lizhaojunleo@gmail.com>
2020-07-08 21:51:17 +0800

    1

---
+++ Solution53-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int i = 0, j = nums.length - 1;
+        while (i <= j) {
+            int m = (i + j) / 2;
+            if (nums[m] == m)
+                i = m + 1;
+            else
+                j = m - 1;
+        }
+        return i;
+    }
+}
+
+// 链接：https://leetcode-cn.com/problems/que-shi-de-shu-zi-lcof/solution/mian-shi-ti-53-ii-0n-1zhong-que-shi-de-shu-zi-er-f/
