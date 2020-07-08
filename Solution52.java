1e31e8f53a00b2453fc829698e5af07efb14b573
leo_plus <lizhaojunleo@gmail.com>
2020-07-08 21:41:49 +0800

    1

---
+++ Solution52.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        // 搜索右边界 right
+        int i = 0, j = nums.length - 1;
+        while (i <= j) {
+            int m = (i + j) / 2;
+            if (nums[m] <= target)
+                i = m + 1;
+            else
+                j = m - 1;
+        }
+        int right = i;
+        // 若数组中无 target ，则提前返回
+        if (j >= 0 && nums[j] != target)
+            return 0;
+        // 搜索左边界 right
+        i = 0;
+        j = nums.length - 1;
+        while (i <= j) {
+            int m = (i + j) / 2;
+            if (nums[m] < target)
+                i = m + 1;
+            else
+                j = m - 1;
+        }
+        int left = j;
+        return right - left - 1;
+    }
+}
+
+// 链接：https://leetcode-cn.com/problems/zai-pai-xu-shu-zu-zhong-cha-zhao-shu-zi-lcof/solution/mian-shi-ti-53-i-zai-pai-xu-shu-zu-zhong-cha-zha-5/
