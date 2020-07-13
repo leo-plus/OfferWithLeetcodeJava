8d9b8deee01464672a5b3713eae462094df07853
leo_plus <lizhaojunleo@gmail.com>
2020-07-14 06:43:10 +0800

    1

--- Solution57-2.java
+++ Solution57-2.java
@@ -1,29 +1,15 @@
 class Solution {
-    public int[][] findContinuousSequence(int target) {
-        int left = 1;
-        int right = 1;
-        int sum = 0;
-        List<int[]> res = new ArrayList<>();
-        while (left <= (target / 2)) {
-            if (sum < target) {
-                sum += right;
-                right++;
-            } else if (sum > target) {
-                sum -= left;
-                left++;
-            } else {
-                int[] array = new int[right - left];
-                for (int i = left; i < right; i++) {
-                    array[i - left] = i;
-                }
-                res.add(array);
-                sum -= left;
-                left++;
+    public String reverseWords(String s) {
+        String[] sArray = s.trim().split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = sArray.length - 1; i >= 0; i--) {
+            if (sArray[i].equals("")) {
+                continue;
             }
+            sb.append(sArray[i]);
+            sb.append(" ");
         }
-        return res.toArray(new int[res.size()][]);
 
+        return sb.toString().trim();
     }
 }
-
-// https://leetcode-cn.com/problems/he-wei-sde-lian-xu-zheng-shu-xu-lie-lcof/solution/shi-yao-shi-hua-dong-chuang-kou-yi-ji-ru-he-yong-h/
