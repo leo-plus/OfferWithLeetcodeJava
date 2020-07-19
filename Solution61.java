9328b6ff2146294f2aad14346cb91c92c87368ce
leo_plus <lizhaojunleo@gmail.com>
2020-07-19 19:55:12 +0800

    1

---
+++ Solution61.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean isStraight(int[] nums) {
+        Arrays.sort(nums);
+        int joker = 0;
+        for (int i = 0; i < 4; i++) {
+            if (nums[i] == 0) {
+                joker++;
+            } else if (nums[i] == nums[i + 1]) {
+                return false;
+            }
+        }
+
+        return nums[4] - nums[joker] < 5;
+
+    }
+}
+
+// 作者：jyd 链接：https://
+// leetcode-cn.com/problems/bu-ke-pai-zhong-de-shun-zi-lcof/solution/mian-shi-ti-61-bu-ke-pai-zhong-de-shun-zi-ji-he-se/
