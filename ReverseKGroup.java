4dba9398657fa864a28e964cdf78e57e23cc72f0
leo-plus <413343061@qq.com>
2020-07-24 22:36:14 +0800

    Create ReverseKGroup.java

---
+++ ReverseKGroup.java
@@ -0,0 +1,38 @@
+public ListNode reverseKGroup(ListNode head, int k) {
+    ListNode dummy = new ListNode(0);
+    dummy.next = head;
+
+    ListNode pre = dummy;
+    ListNode end = dummy;
+
+    while (end.next != null) {
+        for (int i = 0; i < k && end != null; i++) end = end.next;
+        if (end == null) break;
+        ListNode start = pre.next;
+        ListNode next = end.next;
+        end.next = null;
+        pre.next = reverse(start);
+        start.next = next;
+        pre = start;
+
+        end = pre;
+    }
+    return dummy.next;
+}
+
+private ListNode reverse(ListNode head) {
+    ListNode pre = null;
+    ListNode curr = head;
+    while (curr != null) {
+        ListNode next = curr.next;
+        curr.next = pre;
+        pre = curr;
+        curr = next;
+    }
+    return pre;
+}
+
+作者：reals
+链接：https://leetcode-cn.com/problems/reverse-nodes-in-k-group/solution/tu-jie-kge-yi-zu-fan-zhuan-lian-biao-by-user7208t/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
